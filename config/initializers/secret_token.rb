b93090b71b98106990cb2b7a570aaddd80298755
Andrew Wu <wujuichang@gmail.com>
2013-01-02 22:43:06 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fridaylunchspot::Application.config.secret_token = '9b9835a8cfa9e23fbc26f037be8f5dd8a1222f2f6f5a4a6e1589ee0468b998c649eb1cc42049559124255cac1f8bbde1413198962a9ed0648070af1e2f8a8745'
