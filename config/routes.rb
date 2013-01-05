82a5001dc66a917a5faaf1b43c24bb79baa45d2f
Andrew Wu <wujuichang@gmail.com>
2013-01-05 15:29:16 -0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Fridaylunchspot::Application.routes.draw do
+  resources :users
+
   root to: 'static_pages#home'
+
+  match '/signup', to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe "User pages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_selector('h1',    text: 'Sign up') }
+    it { should have_selector('title', text: 'Sign up') }
+  end
+
+  describe "signup" do
+    before { visit signup_path }
+
+    let(:submit) { "Create Account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "First Name",   with: "Example"
+        fill_in "Last Name",    with: "User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
+
+end
