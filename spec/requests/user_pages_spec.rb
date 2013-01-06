08b7bdea2b277f3f3d4d42c171f19872601778ab
Andrew Wu <wujuichang@gmail.com>
2013-01-05 21:01:30 -0800

    Finish user edit and update actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -50,4 +50,33 @@ describe "User pages" do
     end
   end
 
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_selector('h1', text: 'Update your profile') }
+      it { should have_selector('title', text: 'Settings') }
+    end
+
+    describe "with valid information" do
+      let(:new_first_name) { "New" }
+      let(:new_last_name)  { "Name" }
+      before do
+        fill_in "First Name",       with: new_first_name
+        fill_in "Last Name",        with: new_last_name
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { user.reload.first_name.should == new_first_name }
+      specify { user.reload.last_name.should == new_last_name }
+    end
+  end
 end
